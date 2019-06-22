3078087c12081f234850ef0b18ac576997c90591
Shinichi Sato <hyotang.karakoma@gmail.com>
2019-06-22 22:35:55 +0900

    Add: Special variable *VOCABULARY*.

--- src/ojilang-cl.lisp
+++ src/ojilang-cl.lisp
@@ -4,3 +4,6 @@
   (:export))
 (in-package :ojilang-cl)
 
+; quote block progn catch locally go progv symbol-macrolet load-time-value macrolet unwind-protect flet throw eval-when multiple-value-call if return-from let the tagbody setq let* labels multiple-value-prog1 function
+
+(defvar *vocabulary* (make-hash-table))
